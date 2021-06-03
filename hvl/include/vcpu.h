4ceb0b00cf64c6695b7284e5d9d874d012b87151
unisira <owenmc362@gmail.com>
2021-06-03 15:39:00 +0100

    Add some boilerplate code for VCPU and a VMX control toggle wrapper for VCPUs
    Adds an empty vcpu_create function which will be expanded upon later, also adds a wrapper for vmx_toggle_control which is passed the VCPU's VMX state. Also adds the start of a VCPU state structure which will hold the state of any important registers upon VM-exits

--- hvl/include/vcpu.h
+++ hvl/include/vcpu.h
@@ -3,10 +3,44 @@
 
 #include "hvl_types.h"
 #include "config.h"
+#include "vmx.h"
+
+typedef struct
+{
+    // Debug Registers
+    uint64_t dr0; 
+
+    // General purpose registers
+    uint64_t rax;
+    uint64_t rcx;
+    uint64_t rdx;
+    uint64_t rbx;
+    uint64_t rsp;
+    uint64_t rbp;
+    uint64_t rsi;
+    uint64_t rdi;
+    uint64_t r8;
+    uint64_t r9;
+    uint64_t r10;
+    uint64_t r11;
+    uint64_t r12;
+    uint64_t r13;
+    uint64_t r14;
+    uint64_t r15;
+    uint64_t rip;
+} vcpu_state_t;
 
 typedef struct
 {
     uint8_t id;
+
+    // The most recent state of registers from this VCPU
+    vcpu_state_t state;
+
+    // The current state of the VMX controls
+    vmx_state_t vmx;
 } vcpu_t;
+
+hvl_status_t vcpu_toggle_control(vcpu_t*, vmx_control_t);
 
 #endif
--- hvl/vcpu.c
+++ hvl/vcpu.c
@@ -1,1 +1,21 @@
 #include "include/vcpu.h"
+#include "include/hvl_print.h"
+
+vcpu_t* vcpu_create(hvl_config_t* config)
+{
+    return NULL;
+}
+
+// Toggles a VMX control inside of this VCPUs VMX state
+hvl_status_t vcpu_toggle_control(vcpu_t* vcpu, vmx_control_t control)
+{
+    if (vcpu == NULL)
+    {
+        hvl_print("vcpu_toggle_control: Null VCPU pointer\n");
+        return HVL_INVALID_PARAMETER;
+    }
+
+    vmx_toggle_control(&vcpu->vmx, control);
+
+    return HVL_OK;
+}
