ae3452fab015bd7a12da7837b8dd1364d290a224
unisira <owenmc362@gmail.com>
2021-06-03 15:25:36 +0100

    Add true/false definitions, protect NULL macro definition and update and expand upon error code definitions
    Adds true/false macro definitions, protects the NULL macro definition to avoid redefinition when using HVL, and updates error code definitions by adding new ones and changing the HVL_FAILURE macro to HVL_OK

--- hvl/include/hvl_types.h
+++ hvl/include/hvl_types.h
@@ -1,6 +1,8 @@
 #ifndef HVL_TYPES_H
 #define HVL_TYPES_H
 
+#define true 1
+#define false 0
 typedef unsigned char bool;
 
 typedef char int8_t;
@@ -17,11 +19,15 @@ typedef uint32_t hvl_status_t;
 // Strongly typed handle alias for VMs
 typedef uint32_t hvl_handle_t;
 
+#ifndef NULL
 #define NULL (void*)0
+#endif
 
-#define HVL_STATUS_OK 0x00000001
+#define HVL_OK 0x00000000
+#define HVL_INVALID_PARAMETER 0x00000001
+#define HVL_NO_RESOURCES 0x00000002
 
-#define HVL_OK(status) \
-    (status >= HVL_STATUS_OK)
+#define HVL_FAILURE(status) \
+    (status > HVL_OK)
 
 #endif
